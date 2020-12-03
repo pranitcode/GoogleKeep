3e214f2eb2be77afe9730db8a5fc2ea566d049bc
Pranit Ingole <61083651+pranitcode@users.noreply.github.com>
2020-12-03 13:59:09 +0530

    Updates

---
+++ precache-manifest.89d10fad98acf8bc5508b61537d10bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10f56e3bf6246000e1eb32c0d1ab416",
+    "url": "/GoogleKeep/index.html"
+  },
+  {
+    "revision": "b9a69124f14e69de9127",
+    "url": "/GoogleKeep/static/js/2.59430a11.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/GoogleKeep/static/js/2.59430a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1765add981b71252f234",
+    "url": "/GoogleKeep/static/js/main.261241e1.chunk.js"
+  },
+  {
+    "revision": "74ebdb2fea0000a08c09",
+    "url": "/GoogleKeep/static/js/runtime-main.ebfac10e.js"
+  }
+]);
